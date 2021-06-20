6f756eb3ff0317a6cb0f03043c066a989e05bf7f
ka-raja-babu <83501300+ka-raja-babu@users.noreply.github.com>
2021-06-20 12:36:30 +0530

    Add files via upload

--- Assignment9/Codes/Assignment9_2.py
+++ Assignment9/Codes/Assignment9_2.py
@@ -15,13 +15,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Generate line points
-def line_dir_pt(m,A,k1,k2):
+def line_gen(A,B):
   len =10
   dim = A.shape[0]
   x_AB = np.zeros((dim,len))
-  lam_1 = np.linspace(k1,k2,len)
+  lam_1 = np.linspace(0,1,len)
   for i in range(len):
-    temp1 = A + lam_1[i]*m
+    temp1 = A + lam_1[i]*(B-A)
     x_AB[:,i]= temp1.T
   return x_AB
 
