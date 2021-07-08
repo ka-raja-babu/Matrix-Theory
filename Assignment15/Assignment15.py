e8ebdb0b38ae94941a662b97a0990d136d0d94cd
ka-raja-babu <83501300+ka-raja-babu@users.noreply.github.com>
2021-07-08 14:59:54 +0530

    Add files via upload

--- Assignment15/Assignment15.py
+++ Assignment15/Assignment15.py
@@ -12,7 +12,6 @@ Original file is located at
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 #Defining f(x)
 def f(x,a,b,d):
@@ -29,12 +28,6 @@ previous_step_size = 1
 max_iters = 100000000 
 iters = 0
 
-#For minima 
-x1=-2
-x2=4
-x3=4.5
-mn=min(f(x1,a,b,d),f(x2,a,b,d),f(x3,a,b,d))
-
 #Defining derivative of f(x)
 df = lambda x: 2*a*x + b           
 
@@ -45,17 +38,26 @@ while (previous_step_size > precision) & (iters < max_iters) :
     previous_step_size = abs(cur_x - prev_x)   
     iters+=1  
 
+#For minima 
+x1=-2
+x2=4
+x3=4.5
+mn=min(f(x1,a,b,d),f(x2,a,b,d),f(x3,a,b,d))
+
 print("Maximum value of f(x) is ",f(cur_x,a,b,d), "at","x =",cur_x)
 print("Minimum value of f(x) is ",mn, "at","x =",np.roots([a,b,-mn])[1])
 
-# plotting f(x)
+#Plotting f(x)
 x=np.linspace(-3,11,40)
 y=-0.5*(x**2)+4*x
 plt.plot(x,y,label='$4x-0.5x^2$')
+
+#Labelling points
 plt.plot(cur_x,f(cur_x,a,b,d),'o')
 plt.text(cur_x*(1+0.1), f(cur_x,a,b,d)*(1+0.02),'P(4,8)')
 plt.plot(-2,-10,'o')
 plt.text(-1.8,-11,'Q(-2,-10)')
+
 plt.xlabel("x axis")
 plt.ylabel("y axis")
 plt.grid()
